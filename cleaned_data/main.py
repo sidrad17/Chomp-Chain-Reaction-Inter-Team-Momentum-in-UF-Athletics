d0302b84082aeea584b1c29e9238dddd4136a1a6
jessicadippolito <jessdipp@gmail.com>
2025-12-01 12:35:46 -0500

    update

--- cleaned_data/main.py
+++ cleaned_data/main.py
@@ -3,9 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 sb_df = pd.read_csv('softball.csv')
 bb_df = pd.read_csv('baseball.csv')
-#uncomment when CSVs are available
 fb_df = pd.read_csv('football.csv')
-#ten_df = pd.read_csv('tennis.csv')
+ten_df = pd.read_csv('tennis.csv')
 wten_df = pd.read_csv('womens_tennis.csv')
 bk_df = pd.read_csv('basketball.csv')
 wbk_df = pd.read_csv('womens_basketball.csv')
@@ -22,11 +21,10 @@ def retrieve_data_frame(sport):
         return sb_df
     elif sport == 'baseball':
        return bb_df
-    #uncomment as dataframes become available
     elif sport == 'football':
        return fb_df
-    #elif sport == 'tennis':
-       #return ten_df
+    elif sport == 'tennis':
+       return ten_df
     elif sport == "women's tennis":
        return wten_df
     elif sport == 'basketball':
@@ -70,15 +68,15 @@ def comparing_all_sports():
 def stacked_bar_plot():
     xticks = [0]
     xticklabels = ['1925-1926']
-    temp_sport_list = ['baseball','softball',"women's basketball",'volleyball','basketball','football',"women's tennis",'soccer']
+    sport_list = ['baseball','softball',"women's basketball",'volleyball','basketball','football',"women's tennis",'soccer','tennis']
     start_year = 1926
 
     base_list = ['1925-1926']
     columns = ['season']
-    for i in range(0,len(temp_sport_list)):
-        sport_df = retrieve_data_frame(temp_sport_list[i])
+    for i in range(0,len(sport_list)):
+        sport_df = retrieve_data_frame(sport_list[i])
         base_list.append(0)
-        columns.append(temp_sport_list[i])
+        columns.append(sport_list[i])
     df = pd.DataFrame([base_list], columns=columns)
     num_rows = 1
 
@@ -87,18 +85,18 @@ def stacked_bar_plot():
         if int(retrieve_data_frame('football')['year'].iloc[-i][3])==5:
             xticks.append(i-1)
             xticklabels.append(retrieve_data_frame('football')['year'].iloc[-i])
-        for j in range(0,len(temp_sport_list)):
-            sport_df = retrieve_data_frame(temp_sport_list[j])
+        for j in range(0,len(sport_list)):
+            sport_df = retrieve_data_frame(sport_list[j])
             df_start_year = convert_year_to_integer(sport_df['year'].iloc[-1])
             if df_start_year > start_year:
                 if i <= df_start_year - start_year:
                     vals.append(0)
                 else:
-                    if retrieve_data_frame(temp_sport_list[j])['national_championship'].iloc[-i+(convert_year_to_integer(sport_df['year'].iloc[-1]) - start_year)+1] == 'yes':
+                    if retrieve_data_frame(sport_list[j])['national_championship'].iloc[-i+(convert_year_to_integer(sport_df['year'].iloc[-1]) - start_year)+1] == 'yes':
                         vals.append(1)
                     else:
                         vals.append(0)
-            elif retrieve_data_frame(temp_sport_list[j])['national_championship'].iloc[-i] == 'yes':
+            elif retrieve_data_frame(sport_list[j])['national_championship'].iloc[-i] == 'yes':
                 vals.append(1)
             else:
                 vals.append(0)
@@ -117,11 +115,10 @@ def stacked_bar_plot():
 
 #option 1 graph function,
 def champ_sports_comparison(champ_sport):
-    # complete_sport_list = ['football', 'basketball','softball','baseball','gymnastics','volleyball','tennis',"women's tennis",'soccer',"women's basketball"]
-    temp_sport_list = ['softball', 'basketball', 'baseball', 'soccer', 'volleyball', "women's basketball", 'football']
+    sport_list = ['baseball','softball',"women's basketball",'volleyball','basketball','football',"women's tennis",'soccer','tennis']
 
     sports_df = {}
-    for sport in temp_sport_list:
+    for sport in sport_list:
         df = retrieve_data_frame(sport)
         df["start_year"] = df["year"].apply(convert_year_to_integer)
         df["season_label"] = df["year"]
